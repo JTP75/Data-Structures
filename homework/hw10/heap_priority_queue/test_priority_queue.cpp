8c7e40104da67097c2d28f5c44b6070767cefb69
knb-mac <knb12@pitt.edu>
2021-04-10 15:43:15 -0400

    hw10 updates

--- homework/hw10/heap_priority_queue/test_priority_queue.cpp
+++ homework/hw10/heap_priority_queue/test_priority_queue.cpp
@@ -1,14 +1,18 @@
-#include <cassert>
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_COLOUR_NONE
+#include "catch.hpp"
 
-#include "dynamic_array_list.h"
 #include "heap_priority_queue.h"
 
-int main()
-{
+TEST_CASE("Test Construction", "[construction]") {
   HeapPriorityQueue<int>  pq;
   assert(pq.isEmpty());
-  
+}
+
+TEST_CASE("Test add", "[add]") {
+  HeapPriorityQueue<int>  pq;
   pq.add(25);
+
   assert(!pq.isEmpty());
   assert(pq.peek() == 25);
 
@@ -17,11 +21,18 @@ int main()
 
   pq.add(78);
   assert(pq.peek() == 78);
+}
+
+TEST_CASE("Test remove", "[remove]") {
+  HeapPriorityQueue<int>  pq;
+  pq.add(25);
+  pq.add(1);
+  pq.add(78);
 
   pq.remove();
+  assert(pq.peek() == 25);
   pq.remove();
+  assert(pq.peek() == 1);
   pq.remove();
   assert(pq.isEmpty());
-
-  return 0;
 }
