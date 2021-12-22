e6d7748a8dd252eaf094c11d9596adfc3a51d2a0
knb-mac <knb12@pitt.edu>
2021-12-22 16:24:31 -0500

    rearranging and updating cmakelists

--- programming_problems/problem03/fancy_power/fancy_power_tests.cpp
+++ programming_problems/problem03/fancy_power/fancy_power_tests.cpp
@@ -1,10 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "fancyPower.hpp"
+#include "fancy_power.hpp"
 
 // write your test cases here
-TEST_CASE("Test case", "fancyPower"){
+TEST_CASE("Test case", "fancy power"){
 
   REQUIRE(true);
   
