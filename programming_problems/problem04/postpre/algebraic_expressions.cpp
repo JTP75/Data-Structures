03d2fa9f1b267d6ba90e05d375687a4dca19934d
knb-mac <knb12@pitt.edu>
2022-05-10 16:02:59 -0400

    problem 4 now postpre

---
+++ programming_problems/problem04/postpre/algebraic_expressions.cpp
@@ -0,0 +1,48 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+
+#include <cctype> // for isalpha
+
+#include "algebraic_expressions.hpp"
+
+bool isoperator(char ch) {
+  return ((ch == '+') || (ch == '-') || (ch == '/') || (ch == '*'));
+}
+
+int endPost(string s, int last) {
+  int first = 0;
+
+  if ((first > last)) {
+    return -1;
+  }
+
+  char ch = s[last];
+  if (isalpha(ch)) {
+    return last;
+  } else {
+    if (isoperator(ch)) {
+      int lastEnd = endPost(s, last - 1);
+      if (lastEnd > -1) {
+        return endPost(s, lastEnd - 1);
+      } else {
+        return -1;
+      }
+    } else {
+      return -1;
+    }
+  }
+}
+
+bool isPost(string s) {
+  int firstChar = endPost(s, s.size() - 1);
+
+  return (firstChar == 0);
+}
+
+void convert(string &postfix, string &prefix) {
+
+  // TODO
+  
+}
