550cac7eb6c1a6a6aad59b8164e0502da21283a6
knb-mac <knb12@pitt.edu>
2022-05-10 16:04:10 -0400

    problem 5 now sorting

---
+++ programming_problems/problem05/sorting/sort.hpp
@@ -0,0 +1,12 @@
+#ifndef _SORT_HPP
+#define _SORT_HPP
+
+#include "linked_list.hpp"
+
+template<typename T> LinkedList<T> sort(LinkedList<T> list)
+{
+	// your code should go here.
+	return list;
+}
+
+#endif
