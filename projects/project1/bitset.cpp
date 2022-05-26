704431ab16ef141808562f73b81b032ba9379a5e
jtp75 <jtp75@pitt.edu>
2022-05-25 21:19:34 -0400

    All Files Updated

--- projects/project1/bitset.cpp
+++ projects/project1/bitset.cpp
@@ -1,19 +1,61 @@
 #include "bitset.hpp"
 
-Bitset::Bitset() {
-    // TODO
-}
+Bitset::Bitset()
+{
+    BSsize = 8;
+    ADTPtr = new uint8_t[BSsize];
+    isValid = true;
 
-Bitset::Bitset(intmax_t size) {
-    // TODO
+    for(intmax_t i=0; i<BSsize; i++)
+        *(ADTPtr+i) = 0;
 }
-
-Bitset::Bitset(const std::string & value) {
-    // TODO
+Bitset::Bitset(intmax_t sz)
+{
+    isValid = sz > 0;
+    BSsize = sz;
+    ADTPtr = new uint8_t[BSsize];
+    for(intmax_t i=0; i<BSsize; i++)
+        *(ADTPtr+i) = 0;
 }
-
-Bitset::~Bitset() {
-    // TODO
+Bitset::Bitset(const std::string &value)
+{
+    BSsize = value.length();
+    ADTPtr = new uint8_t[BSsize];
+    isValid = true;
+    const uint8_t* tmpPtr = reinterpret_cast<const uint8_t*>(value.c_str());
+    for(intmax_t i=0; i<BSsize; i++)
+    {
+        isValid = isValid && (value[i]==1 || value[i]==0);
+        *(ADTPtr+i) = *(tmpPtr+i);
+    }
 }
-
-// TODO: other methods
+Bitset::~Bitset(){delete [] ADTPtr;}
+intmax_t Bitset::size() const {return BSsize;}
+bool Bitset::good() const {return isValid;}
+void Bitset::set(intmax_t index)
+{
+    isValid = isValid && (index >= 0 || index < BSsize);
+    *(ADTPtr+index) = 1;
+}
+void Bitset::reset(intmax_t index)
+{
+    isValid = isValid && (index >= 0 || index < BSsize);
+    *(ADTPtr+index) = 0;
+}
+void Bitset::toggle(intmax_t index)
+{
+    isValid = isValid && (index >= 0 || index < BSsize);
+    *(ADTPtr+index) = *(ADTPtr+index)==1 ? 0 : 1;
+}
+bool Bitset::test(intmax_t index)
+{
+    isValid = isValid && (index >= 0 || index < BSsize);
+    return isValid && *(ADTPtr+index)==1;
+}
+std::string Bitset::asString() const
+{
+    std::string s = "";
+    for(intmax_t i=0; i<BSsize; i++)
+        s += std::to_string(*(ADTPtr+i));
+    return s;
+}
--- projects/project1/bitset.hpp
+++ projects/project1/bitset.hpp
@@ -4,48 +4,26 @@
 #include <stdlib.h>
 #include <string>
 
-class Bitset{
-public:
+class Bitset
+{
+  public:
+    Bitset();                                       // dynamically allocates memory for uint8_t array of length 8, valid
+    Bitset(intmax_t sz);                            // same as default, but length = sz and validates only if sz > 0
+    Bitset(const std::string &value);               // reads string to array, checking for non-one-or-zero bits
+    ~Bitset();                                      // deletes allocated memory and destroys object
+    Bitset & operator=(const Bitset &) = delete;    // overloads equals operator to compare bitsets
+    intmax_t size() const;                          // returns size
+    bool good() const;                              // returns validity
+    void set(intmax_t index);                       // sets value at index, invalid if index is out of bounds
+    void reset(intmax_t index);                     // resets value at index, invalid if index is OOB
+    void toggle(intmax_t index);                    // toggles value at index, invalid if index is OOB
+    bool test(intmax_t index);                      // returns boolean state at index, invalid if index is OOB
+    std::string asString() const;                   // returns string representation of bitset
 
-  // TODO COMMENT
-  Bitset();
-
-  // TODO COMMENT
-  Bitset(intmax_t size);
-
-  // TODO COMMENT
-  Bitset(const std::string & value);
-
-  // TODO COMMENT
-  ~Bitset();
-
-  Bitset(const Bitset & ) = delete;
-  Bitset & operator=(const Bitset &) = delete;
-
-  // TODO COMMENT
-  intmax_t size() const;
-
-  // TODO COMMENT
-  bool good() const;
-
-  // TODO COMMENT
-  void set(intmax_t index);
-
-  // TODO COMMENT
-  void reset(intmax_t index);
-
-  // TODO COMMENT
-  void toggle(intmax_t index);
-
-  // TODO COMMENT
-  bool test(intmax_t index);
-
-  // TODO COMMENT
-  std::string asString() const;
-
-private:
-
-  // TODO
+  private:
+    uint8_t *ADTPtr;                                // pointer to first uint8_t element of array
+    intmax_t BSsize;                                // length of array
+    bool isValid;                                   // validity
 };
 
 #endif
